c9b380f0f5b2bd8af816fe239946823c766d5163
Ulises Basurto <ulisesbasurto01@gmail.com>
2021-07-19 13:30:00 -0500

    The others services

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,31 @@ import { CustomersController } from './controllers/customers.controller';
 import { UsersController } from './controllers/users.controller';
 import { OrdersController } from './controllers/orders.controller';
 import { ProductsService } from './services/products/products.service';
+import { CategoriesService } from './services/categories/categories.service';
+import { BrandsService } from './services/brands/brands.service';
+import { CustomersService } from './services/customers/customers.service';
+import { UsersService } from './services/users/users.service';
+import { OrdersService } from './services/orders/orders.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController, BrandsController, CustomersController, UsersController, OrdersController],
-  providers: [AppService, ProductsService],
+  controllers: [
+    AppController,
+    ProductsController,
+    CategoriesController,
+    BrandsController,
+    CustomersController,
+    UsersController,
+    OrdersController,
+  ],
+  providers: [
+    AppService,
+    ProductsService,
+    CategoriesService,
+    BrandsService,
+    CustomersService,
+    UsersService,
+    OrdersService,
+  ],
 })
 export class AppModule {}
--- src/common/parse-int.pipe.spec.ts
+++ src/common/parse-int.pipe.spec.ts
@@ -1,7 +1,7 @@
-import { ParseIntPipe } from './parse-int.pipe';
+import { MineParseIntPipe } from './parse-int.pipe';
 
 describe('ParseIntPipe', () => {
   it('should be defined', () => {
-    expect(new ParseIntPipe()).toBeDefined();
+    expect(new MineParseIntPipe()).toBeDefined();
   });
 });
